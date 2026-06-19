9b509c3ea139915980b716cec0fe303a245620e2
Fuxi <1538130391@qq.com>
2026-06-19 17:47:12 +0800

    chap2-BatchOS

--- os/src/main.rs
+++ os/src/main.rs
@@ -1,11 +1,18 @@
 //! The main module and entrypoint
 //!
-//! The operating system and app also starts in this module. Kernel code starts
+//! Various facilities of the kernels are implemented as submodules. The most
+//! important ones are:
+//!
+//! - [`trap`]: Handles all cases of switching from userspace to the kernel
+//! - [`syscall`]: System call handling and implementation
+//!
+//! The operating system also starts in this module. Kernel code starts
 //! executing from `entry.asm`, after which [`rust_main()`] is called to
-//! initialize various pieces of functionality [`clear_bss()`]. (See its source code for
+//! initialize various pieces of functionality. (See its source code for
 //! details.)
 //!
-//! We then call [`println!`] to display `Hello, world!`.
+//! We then call [`batch::run_next_app()`] and for the first time go to
+//! userspace.
 
 #![deny(missing_docs)]
 #![deny(warnings)]
@@ -19,24 +26,34 @@ macro_rules! linker_symbol_addr {
 }
 
 use core::arch::global_asm;
-use log::*;
 
+use log::*;
 #[macro_use]
 mod console;
+pub mod batch;
 mod lang_items;
 mod logging;
 mod sbi;
+mod sync;
+pub mod syscall;
+pub mod trap;
 
 global_asm!(include_str!("entry.asm"));
+global_asm!(include_str!("link_app.S"));
 
 /// clear BSS segment
-pub fn clear_bss() {
+fn clear_bss() {
     unsafe extern "C" {
         safe fn sbss();
         safe fn ebss();
     }
-    (linker_symbol_addr!(sbss)..linker_symbol_addr!(ebss))
-        .for_each(|a| unsafe { (a as *mut u8).write_volatile(0) });
+    unsafe {
+        core::slice::from_raw_parts_mut(
+            linker_symbol_addr!(sbss) as *mut u8,
+            linker_symbol_addr!(ebss) - linker_symbol_addr!(sbss),
+        )
+        .fill(0);
+    }
 }
 
 /// the rust entry-point of os
@@ -82,8 +99,7 @@ pub fn rust_main() -> ! {
         linker_symbol_addr!(sbss),
         linker_symbol_addr!(ebss)
     );
-
-    // CI autotest success: sbi::shutdown(false)
-    // CI autotest failed : sbi::shutdown(true)
-    sbi::shutdown(false)
+    trap::init();
+    batch::init();
+    batch::run_next_app();
 }
